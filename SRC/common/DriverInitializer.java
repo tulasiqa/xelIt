0520a98063fe642f4273baf6a8dd5644f049e76e
tulasiqa <kumaar2514@gmail.com>
2018-11-28 16:22:29 +0530

    reosved all fuctioanlity

--- SRC/common/DriverInitializer.java
+++ SRC/common/DriverInitializer.java
@@ -1,106 +1,99 @@
 package common;
 
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.firefox.internal.ProfilesIni;
 
 public class DriverInitializer extends ExtentReportNG {
+	
 
-  public static WebDriver driver;
-  public static ExtentReports extent;
-  public DesiredCapabilities capabilities;
+	public DesiredCapabilities capabilities;
+	// public static ExtentReports extent;
+	public static final ThreadLocal<RemoteWebDriver> webDriver = new ThreadLocal<RemoteWebDriver>();
 
-  @Parameters({ "browser", "appUrl" })
-  @BeforeSuite
+	@Parameters({ "browser", "appUrl" })
+	@BeforeTest
 
-  public WebDriver openBrowser(String browser, String appUrl) throws MalformedURLException {
+	public WebDriver openBrowser(String browser, String appUrl) throws MalformedURLException {
+		// launch the specified browser
 
-    // launch the specified browser
+		try {
+			if (browser.equalsIgnoreCase("Firefox")) {
+				System.setProperty("webdriver.gecko.driver", gecko_driver);
+				FirefoxProfile fprofile = new FirefoxProfile();
+				fprofile.setAcceptUntrustedCertificates(true);
+				fprofile.setAssumeUntrustedCertificateIssuer(false);
+				fprofile.setPreference("browser.download.dir", filepath);
+				fprofile.setPreference("browser.download.folderList", 2);
+				fprofile.setPreference("browser.download.manager.showWhenStarting", false);
+				fprofile.setPreference("pdfjs.disabled", true);
+				fprofile.setPreference("browser.helperApps.neverAsk.saveToDisk",
+						"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;" + "application/pdf;" 
+						+"image/jpeg"+ "application/msword" + "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+						+ "application/video/mp4" + "application/zip"
+						+ "application/wav"+ "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+						+ "application/vnd.openxmlformats-officedocument.wordprocessingml.document;"
+						+ "text/plain;" // MIME types Of text File.
+						+ "text/csv"); // MIME types Of CSV File.
+				fprofile.setPreference("geo.enabled", false);
+				fprofile.setPreference("geo.provider.use_corelocation", false);
+				fprofile.setPreference("geo.prompt.testing", false);
+				fprofile.setPreference("geo.prompt.testing.allow", false);
+				fprofile.setPreference("permissions.default.desktop-notification", 1);
+				capabilities = DesiredCapabilities.firefox();
+				capabilities.setCapability("marionette", true);
+				new FirefoxOptions().setProfile(fprofile).addTo(capabilities);
+				capabilities.setPlatform(Platform.WINDOWS);
+				capabilities.setBrowserName("firefox");
 
-    try {
-      if (browser.equalsIgnoreCase("Firefox")) {
-    	  System.setProperty("webdriver.gecko.driver",gecko_driver);
-    	//  ProfilesIni prof = new ProfilesIni();  // added new	
-        FirefoxProfile fprofile = new FirefoxProfile();
-        fprofile.setAcceptUntrustedCertificates(true); 
-        fprofile.setAssumeUntrustedCertificateIssuer(false);
-       
-        // Set Location to store files after downloading.
-        fprofile.setPreference("browser.download.dir", filepath);
-        fprofile.setPreference("browser.download.folderList", 2);
-        fprofile.setPreference("browser.download.manager.showWhenStarting",false);
-        // Set Preference to not show file download confirmation dialogue using
-        // MIME types Of different file extension types.
-        fprofile.setPreference("browser.helperApps.neverAsk.saveToDisk",
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;"// MIME
-                                                                                // types
-                                                                                // Of
-                                                                                // MS
-                                                                                // Excel
-                                                                                // File.
-                + "application/pdf;" // MIME types Of PDF File.
-                + "application/vnd.openxmlformats-officedocument.wordprocessingml.document;" // MIME
-                                                                                             // types
-                                                                                             // Of
-                                                                                             // MS
-                                                                                             // doc
-                                                                                             // File.
-                + "text/plain;" // MIME types Of text File.
-                + "text/csv"); // MIME types Of CSV File.
-        fprofile.setPreference("browser.download.manager.showWhenStarting", false);
-        fprofile.setPreference("pdfjs.disabled", true);
-        capabilities =  new FirefoxOptions().setLogLevel(Level.OFF).addTo(DesiredCapabilities.firefox());
-        capabilities.setCapability("marionette", true);
-        capabilities.setCapability(FirefoxDriver.PROFILE, fprofile);
-        capabilities.setPlatform(Platform.WINDOWS);
-        capabilities.setBrowserName("firefox");
-        // Pass fprofile parameter In webdriver to use preferences to download
-        // file.
-        driver = new FirefoxDriver(fprofile);
-        driver.manage().window().maximize();
-        driver.get(appUrl);
-        return driver;
+			} else if (browser.equalsIgnoreCase("chrome")) {
+				System.setProperty("webdriver.chrome.driver", chrome_driver);
 
-      } else if (browser.equalsIgnoreCase("chrome")) {
-        System.setProperty("webdriver.chrome.driver", chrome_driver);
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(appUrl);
-        return driver;
-      } else if (browser.equalsIgnoreCase("ie")) {
-        System.setProperty("webdriver.ie.driver", ie_driver);
-        driver = new InternetExplorerDriver();
-        driver.manage().window().maximize();
-        driver.get(appUrl);
-        return driver;
-      }
-    } catch (Exception e) {
-      e.getMessage();
-    }
-    return driver;
-  }
+				HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+				chromePrefs.put("profile.default_content_settings.popups", 0);
+				chromePrefs.put("download.default_directory", filepath);
+				ChromeOptions options = new ChromeOptions();
+				HashMap<String, Object> chromeOptionsMap = new HashMap<String, Object>();
+				options.setExperimentalOption("prefs", chromePrefs);
+				capabilities = DesiredCapabilities.chrome();
+				capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptionsMap);
+				capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+				capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+				capabilities.setPlatform(Platform.WINDOWS);
+				capabilities.setBrowserName("chrome");
+				
 
-  public static WebDriver driver() {
-    return driver;
-  }
+			} else if (browser.equalsIgnoreCase("ie")) {
+				System.setProperty("webdriver.ie.driver", ie_driver);
+				capabilities = DesiredCapabilities.internetExplorer();
+				capabilities.setPlatform(Platform.WINDOWS);
+
+			}
+			webDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities));
+			driver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver().manage().window().maximize();
+			webDriver.get().get(appUrl);
+			return webDriver.get();
+
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return webDriver.get();
+	}
+
+	public WebDriver driver() {
+		return webDriver.get();
+	}
 
 }
